ef49e7acc1e40a570ab42a40bf442739f5fb4219
Gia Huy Nguyen <gia.huynguyen@bcn.group>
2026-03-13 23:28:02 +0100

    feat: update Firebase authentication to use AsyncStorage for persistence (Don't have to login again when start the app)

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 // Firebase Console → Project Settings → General → Your apps → Web app
 const firebaseConfig = {
@@ -13,5 +14,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
 export const db = getFirestore(app);
